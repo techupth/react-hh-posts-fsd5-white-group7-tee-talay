86f1081e84bef3e82d11d15a83038aabb0c66882
Pacharadanai <pacharadanaipinake@gmail.com>
2023-12-22 11:36:09 +0700

    feat: add completed exercise

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,44 @@
+import postDetail from "../data/post-detail";
+import { useState } from "react";
+
 function Posts() {
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {postDetail.map((item, index) => {
+          const [like, setLike] = useState(item.likes);
+
+          const addLike = () => {
+            setLike(like + 1);
+          };
+
+          const disLike = () => {
+            if (like > 0) {
+              setLike(like - 1);
+            }
+          };
+          return (
+            <div class="post-item" key={index}>
+              <div class="post-header">
+                <h2>{item.title}</h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">{like}</span>
+                </div>
+              </div>
+              <p class="post-content">{item.content}</p>
+              <div class="post-actions">
+                <button class="like-button" onClick={addLike}>
+                  Like
+                </button>
+                <button class="dislike-button" onClick={disLike}>
+                  Dislike
+                </button>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
